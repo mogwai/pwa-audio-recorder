e8c070af9387746daa0163b69ac4a098c3ad0acf
Harry Blum <himion0@gmail.com>
2019-05-13 14:44:26 +0100

    build folder

---
+++ build/precache-manifest.1466c0d975d88763bd557c82533d39bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad301761ab5590f706863438b7b8dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62361abdd8a0eff9d68b",
+    "url": "/static/css/main.d6f33740.chunk.css"
+  },
+  {
+    "revision": "e65712435ce17a36ec96",
+    "url": "/static/js/2.9d9a96b1.chunk.js"
+  },
+  {
+    "revision": "62361abdd8a0eff9d68b",
+    "url": "/static/js/main.16e813b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
