e7efb1a61a9525f1dd161ebdc0848ab2924f5707
Harry Blum <himion0@gmail.com>
2019-05-13 14:54:28 +0100

    Fizedx links

---
+++ build/precache-manifest.3c0470dd0f36d63e717de670b611f9e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1474826cf348b2ec207311008da23d",
+    "url": "/pwa-audio-recorder/index.html"
+  },
+  {
+    "revision": "99743c4d7aa8eb035236",
+    "url": "/pwa-audio-recorder/static/css/main.d6f33740.chunk.css"
+  },
+  {
+    "revision": "e65712435ce17a36ec96",
+    "url": "/pwa-audio-recorder/static/js/2.9d9a96b1.chunk.js"
+  },
+  {
+    "revision": "99743c4d7aa8eb035236",
+    "url": "/pwa-audio-recorder/static/js/main.49ae20c9.chunk.js"
+  },
+  {
+    "revision": "875ba8e961193ca24b88",
+    "url": "/pwa-audio-recorder/static/js/runtime~main.09170c18.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1466c0d975d88763bd557c82533d39bc.js"
+  "/pwa-audio-recorder/precache-manifest.3c0470dd0f36d63e717de670b611f9e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pwa-audio-recorder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
