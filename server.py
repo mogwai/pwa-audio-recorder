d2e65aa0647bf78138c89728edb86f0da6f4fea1
h <himion0@gmail.com>
2020-07-17 18:55:17 +0100

    Made work with raw torchaudio

--- server.py
+++ server.py
@@ -1,51 +1,27 @@
 import os
 import hashlib
 from flask import Flask, Response, request
-from fastai.basic_train import load_learner
-from exp.nb_AudioCommon import AudioData
-from exp.nb_DataBlock import AudioItem
-
-LEARN_PATH = './'
-LEARN_NAME = 'export.pkl'
+import torchaudio
 
 app = Flask(__name__)
-learn = load_learner(LEARN_PATH, LEARN_NAME)
-
-
-def predict(path):
-    ad = AudioData.load(path)
-    res = learn.predict(AudioItem(ad))
-    print(res)
-    """
-    FILL ME IN
-    """
-    return "Not sure :\ "
-
 
 @app.route('/', methods=['POST'])
 def receieve_wav():
     blob = request.files['data']
     filename = request.form['fname'] or 'recording'
-    # buf = blob.read()
-    # hasher = hashlib.md5()
-    # hasher.update(buf)
-    # md5 = hasher.hexdigest()
     s = filename
     filename = s+"."+blob.content_type.replace("audio/", "")
     path = os.path.join('./', filename)
 
     blob.save(path)
-    resp = Response(predict(path))
-
-    return resp
-
+    sig, sr = torchaudio.load(path)
+    print(sig.shape, sr)
+    return str(sig.shape)
 
 @app.after_request
 def apply_caching(resp):
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
-
 if __name__ == "__main__":
-
     app.run(host='0.0.0.0', port=8080)
